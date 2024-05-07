55869bf0534f01a726c3dc92f0c644cbab1f7751
Viacheslav Shalisko <vshalisko@users.noreply.github.com>
2024-05-07 14:11:28 -0600

    Update convert_gee_image_to_numpy_array_functions.py

--- Colab/convert_gee_image_to_numpy_array_functions.py
+++ Colab/convert_gee_image_to_numpy_array_functions.py
@@ -1,4 +1,9 @@
+import ee
+import numpy as np
+from pyrsgis import raster, convert
+
 ## functions related to conversion of GEE image to mumpy array, in segmented fasion
+
 def segmentation(limits, pixel_size, max_tile_size, proj):
   alto = math.ceil((limits[3] - limits[2]) / pixel_size)
   print('Alto del segmento (pixeles)', alto)
@@ -32,7 +37,18 @@ def tile_to_numpy(image, rectangle, bands):
     band_array_np = np.concatenate((band_array_np, b_arr_np), 2)
   return band_array_np
 
-limites_utm = [640453, 702758, 2260275, 2317848]
+def merge_feature_array(image, segments, bands):
+  ## combinaccion de segmentos
+  array_feature = tile_to_numpy(image, segments[0], bands)
+  print("New feature array", array_feature.shape)
+  for segment in segments[1:]:
+    feature_segment = tile_to_numpy(image, segment, bands)
+    print("Feature segment", feature_segment.shape)
+    array_feature = np.concatenate((array_feature, feature_segment), 1)
+  print("Full new feature array", array_feature.shape)
+  return array_feature
+
+limites_utm = [640000, 702800, 2255000, 2310000]
 pixel = 30
 max_tile = 250000
 utm13 = ee.Projection('EPSG:32613')
@@ -40,19 +56,5 @@ bands_L8 = ['SR_B2','SR_B3','SR_B4','SR_B5','SR_B6','SR_B7']
 bands_B = ['settlement']
 
 segment_list = segmentation(limites_utm, pixel, max_tile, utm13)
-
-array_feature = tile_to_numpy(L8_image, segment_list[0], bands_L8)
-print("New feature array", array_feature.shape)
-for segment in segment_list[1:]:
-  feature_segment = tile_to_numpy(L8_image, segment, bands_L8)
-  print("Feature segment", feature_segment.shape)
-  array_feature = np.concatenate((array_feature, feature_segment), 1)
-print("Full new feature array", array_feature.shape)
-
-array_target = tile_to_numpy(built_2015_hr_reprojected, segment_list[0], bands_B)
-print("New label array", array_target.shape)
-for segment in segment_list[1:]:
-  target_segment = tile_to_numpy(built_2015_hr_reprojected, segment, bands_B)
-  print("Label segment", target_segment.shape)
-  array_target = np.concatenate((array_target, target_segment), 1)
-print("Full new label array", array_target.shape)
+array_feature_2015 = merge_feature_array(L8_image_2015, segment_list, bands_L8)
+array_target_2015 = merge_feature_array(built_2015_hr_reprojected, segment_list, bands_B)
